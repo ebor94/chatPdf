72edb0f7b4badb610e69e8901b8302a205fc6561
ebor94 <webmaster@ceramicaitalia.com>
2023-09-21 17:13:06 -0500

    config ssl server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import  Express  from "express";
 import bodyParser from "body-parser";
 import Getfile from "./routes/chat.route.js";
+import https from "https";
+import fs from "fs";
 //import sendMessage  from "./routes/chat.route.js";
 import  {sendMessages}  from "./controllers/sendMessage.js";
+const hostname = "ceramicaitalia.com"
 const app = Express();
 const port  = 3000
+
 app.use((req, res, next) => {
 
     // Dominio que tengan acceso 
@@ -29,6 +33,15 @@ app.post('/sendMessage/', async (req,res)=>{
   const response =  await sendMessages(req.body)
   res.send(response);
 }) 
- app.listen(port, () => {
+
+const options = {
+   key : fs.readFileSync("/etc/pki/SSL_cert/wildcard_ceramicaitalia_com.key"),
+   cert: fs.readFileSync("/etc/pki/SSL_cert/wildcard_ceramicaitalia_com.crt"),
+   ca  : fs.readFileSync("/etc/pki/SSL_cert/DigiCertCA.crt")
+};
+
+
+
+https.createServer(options, app).listen(port, () => {
     console.log(`chatPdf listening on port ${port}`)
   })
